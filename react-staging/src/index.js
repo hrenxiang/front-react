7487d9942ea6b73c7e28fa446fa7da1acaef9156
huangrx <2295701930@qq.com>
2023-04-18 17:55:09 +0800

    add: react staging init

---
+++ react-staging/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import HelloWorld from './components/HelloWorld';
+import './style.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App/>
+        <HelloWorld/>
+    </React.StrictMode>
+);
